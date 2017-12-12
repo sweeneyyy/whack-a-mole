2956b9ab2bbe6130084b00782d9a53148d3a9542
sweeneyyy <sweeneyarnett@gmail.com>
2017-12-11 17:24:14 -0800

    updated with assets

---
+++ script.js
@@ -0,0 +1,29 @@
+var sharkSpaces = document.querySelectorAll("#gameGrid div");
+var player1Score = 0;
+var player2Score = 0;
+var highScore;
+
+
+
+// Change grid box background image randomly during game from ocean to shark
+
+
+// Set time limit of the game
+
+	// Display timer
+
+
+// Set time shark image is displayed before switching back to ocean
+
+
+// Keep track of score
+
+	// If player clicks on shark image before it disappears, add 1 point to score
+	// If player does not click on the shark image while displayed, add 1 to missed
+
+
+	// Display score and missed 
+
+
+// Save high score in local storage and stay displayed on board
+
