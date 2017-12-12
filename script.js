ed373bca973cc12c6a1d66d08e63ebbf595e63d7
sweeneyyy <sweeneyarnett@gmail.com>
2017-12-11 22:19:06 -0800

    attempt at timer

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+var interval;
+var time = 10;
 var sharkSpaces = document.querySelectorAll("#gameGrid div");
 var player1Score = 0;
 var player2Score = 0;
 var highScore;
 
 
+document.addEventListener("DOMContentLoaded", function() {
 
-// Change grid box background image randomly during game from ocean to shark
+
+// On click of Start Game - Change grid boxes background image randomly from ocean to shark
 
 
 // Set time limit of the game
 
-	// Display timer
+var countdown = function(){
+	time -= 1;
+	console.log("time left", countdown);
+ 	// Display timer
+
+	// document.getElementById("#timer").textContent = time;
+
+
+};
+
+countdown();
 
 
 // Set time shark image is displayed before switching back to ocean
@@ -27,3 +41,20 @@ var highScore;
 
 // Save high score in local storage and stay displayed on board
 
+
+
+// Add Event Listeners
+	// document.getElementById("reset").addEventListener("click", reset);
+	// call function that is sharkBoxes event listeners
+
+	// Start the timer
+	interval = setInterval(countdown, 1000);
+
+
+	// Start jaws theme song?
+	
+
+	// Set up the sharkBoxes
+
+	
+});
