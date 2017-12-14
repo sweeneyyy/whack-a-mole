98d5178ffd8015c33c45cf0f9740a759ed2a3807
sweeneyyy <sweeneyarnett@gmail.com>
2017-12-14 13:54:49 -0800

    working game needs style

--- script.js
+++ script.js
@@ -9,34 +9,49 @@ var currentScore = 0;
 var player1Score = 0;
 var player2Score = 0;
 
-// if(localStorage.getItem("highScore") === undefined){
-//    localStorage.setItem("highScore", 0);
-//    console.log(localStorage);
-// }
+// var removeSharkSpacesEventListeners = function(){
+// 	for(var i = 0; i < sharkSpaces.length; i++){
+// 		sharkSpaces[i].removeEventListener("click", gameScore);
+//   }
+// };
+var newGame = function() {
+	totalSeconds = 10;
+	player1Score = 0;
+	player2Score = 0;
+	addSharkSpacesEventListeners();
 
+	document.getElementById("score-1").textContent = player1Score;
+	document.getElementById("score-2").textContent = player2Score;
+	document.getElementById("start").style.display = "block";
+	document.getElementById("start").textContent = "Player 1";
+	document.getElementById("reset").style.display = "none";
+}
+// Start new game
+var startGame = function(){
+	totalSeconds = 10;
+	countdown();
+  // Set timer to zero for new game
+	clearInterval(interval);
+	interval = setInterval(countdown, 1000);
+	// Set player 1 or 2 for new game
+	playerTurn();
+};
+
+// Swith turns between player 1 and 2 and dislplay current player
 var playerTurn = function(){
 	if(currentPlayer === 1){
 		currentPlayer = currentPlayer + 1;
 		currentScore = 0;
 		document.getElementById("playerUp").textContent = "Player 2";
+		document.getElementById("start").style.display = "none";
+		document.getElementById("reset").style.display = "block";
+
 	}else {
 		currentPlayer = currentPlayer - 1;
 		document.getElementById("playerUp").textContent = "Player 1";
+		document.getElementById("start").textContent = "Player 2";
 	}
 };
-
-var startGame = function(){
-	totalSeconds = 10;
-	countdown();
-
-	clearInterval(interval);
-	interval = setInterval(countdown, 1000);
-
-	playerTurn();
-
-
-};
-
 // Change grid boxes background image randomly from ocean to shark
 var displaySharks = function(spaceToPopulate){
 	// Set everything back to waves
@@ -49,11 +64,11 @@ var displaySharks = function(spaceToPopulate){
 	spaceToPopulate.classList.remove("water");
 };
 
-// Set time limit of the game
+// Set time limit of the game and increment
 var countdown = function(){
 	totalSeconds -= 1;
 	document.getElementById("timer").textContent = totalSeconds;
-
+	// Set grid back to all water, no sharks
 	for(var i = 0; i < sharkSpaces.length; i++){
 	  sharkSpaces[i].classList.remove("shark");
 	  sharkSpaces[i].classList.add("water");
@@ -78,18 +93,18 @@ var gameScore = function(whack){
 		if(currentPlayer === 1){
 			player1Score++;
 			document.getElementById("score-1").textContent = player1Score;
-			console.log("player 1 score is", player1Score);
+				console.log("player 1 score is", player1Score);
 
 		}else{
 			player2Score++;
 			document.getElementById("score-2").textContent = player2Score;
-			console.log("player 2 score is", player2Score);
+				console.log("player 2 score is", player2Score);
 		}
 	}
 };
 
 var checkHighScore = function(){
-	console.log(localStorage.getItem("highScore"));
+	// console.log(localStorage.getItem("highScore"));
 	if(player1Score > localStorage.getItem("highScore")){
 		highScore = player1Score;
 		document.getElementById("highScore").textContent = highScore;
@@ -103,7 +118,6 @@ var checkHighScore = function(){
 			console.log("P2 highScore is ", highScore);
 	}	
 };
-
 
 // Who's score was higher?
 var checkWinner = function(){
@@ -122,7 +136,7 @@ var addSharkSpacesEventListeners = function(){
 	for(var i = 0; i < sharkSpaces.length; i++){
 		sharkSpaces[i].addEventListener("click", gameScore);
   }
-}
+};
 
 addSharkSpacesEventListeners();
 
@@ -130,14 +144,17 @@ addSharkSpacesEventListeners();
 document.addEventListener("DOMContentLoaded", function() {
 // Add Event Listeners
 if(localStorage.getItem("highScore")){
-		document.getElementById("highScore").textContent = localStorage.getItem("highScore");
+	document.getElementById("highScore").textContent = localStorage.getItem("highScore");
 	}
-	// document.getElementById("reset").addEventListener("click", reset);
+	document.getElementById("reset").addEventListener("click", newGame);
+	document.getElementById("reset").style.display = "none";
 	document.getElementById("start").addEventListener("click", startGame);
-	// call function that is sharkBoxes event listeners
 
 	// Start jaws theme song?	
 
 	// Set up the sharkBoxes
 	
 });
+
+// this.removeEventListener("click", gameScore);
+// 			console.log(this);
